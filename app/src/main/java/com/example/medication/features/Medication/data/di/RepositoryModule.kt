4cfd7726fbdb32aa65e1a0f0c0da9ece490d296c
Luis1f2 <221271@ids.upchiapas.edu.mx>
2026-03-11 12:36:05 -0600

    Actualizacion_rama_segunda

---
+++ app/src/main/java/com/example/medication/features/Medication/data/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.example.Medication.features.Medication.data.di
+
+import com.example.medication.features.Medication.data.repositories.MedicationRepositoryImpl
+import com.example.medication.features.medication.domain.repositories.MedicationRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun bindMedicationRepository(
+        impl: MedicationRepositoryImpl
+    ): MedicationRepository
+}
